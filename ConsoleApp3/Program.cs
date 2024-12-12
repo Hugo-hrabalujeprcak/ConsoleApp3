dd59c7a0086f1f7e9515416159e5a53774fd5806
h.hrabal <h.hrabal@PC-IVT11>
2024-12-12 17:28:10 +0900

    Přidejte soubory projektu.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Zadej kolik ti je");
+string prectenyRadek = Console.ReadLine();  
+int vek = Convert.ToInt32(prectenyRadek);
+int odchodDoDuchodu = 67 - vek;
+if(vek > 65)
+{
+    Console.WriteLine("Jses boomer");
+}
+else
+{
+    Console.WriteLine(" Ještě něco chybí ..." + odchodDoDuchodu);
+}
